e23e9497485e10fb6ca18351960ff33aebe41a31
Agility6 <agility6@163.com>
2023-02-20 21:12:27 +0800

    traverse update

--- Tree/tree-traversal.cpp
+++ Tree/tree-traversal.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #define OK 1
+#define NULL 0
 
 /**
   注意：该代码为伪代码!
@@ -76,6 +77,66 @@ Status PostOrderTraverse(BiTree T) {
   InOrderTraverse(T -> rchild)
   // Access the root node
   visit(T);
+  
+}
+
+/**
+  InOrderTraverse_2
+  use Stack
+*/
+Status InOrderTraverse_2(BiTree T) {
+   
+   // InitStack
+   InitStack(S);
+   BiTree *p, *q;
+   p = T;
+
+  /**
+    StackEmpty: TRUE
+    NotStackEmpty: FALSE
+  */
+  while(p || !StackEmpty(S)) {
+    if(p) {
+      Push(S,p);
+      p = p -> lchild;
+    } else {
+      Pop(S, q);
+      printf("%d", q -> data);
+      p = q -> rchild;
+    }
+  }
+
+  return OK;
+}
+
+/**
+  将根节点进队
+  对不空時循环：从队列中出列一个结点*p,访问
+    1. 若有左孩子结点，将左孩子结点进队
+    2. 若由右孩子结点，将右孩子结点进队
+*/
+void LevelOrder(BTNode *b) {
+
+  BTNode *p;
+  SqQueue *qu;
+
+  // Init
+  InitQueue(qu);
+  enQueue(qu,b); // 根结点入队
+
+  // 队不为空
+  while(!QueueEmpty(qu)) {
+    // 出队结点p
+    deQueue(qu, p);
+    printf("%d", p -> data);
+    if(p -> lchild != NULL) {
+      // 左孩子入队
+      enQueue(qu, p -> lchild)''
+    }
+    if(p -> rchild != NULL) {
+      enQueue(qu, p -> rchild);
+    }
+  }
   
 }
 
