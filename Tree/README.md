64150b1254394e12d588493c4ae21dc62530a956
Agility6 <agility6@163.com>
2023-02-20 23:36:35 +0800

    readme app-tree

---
+++ Tree/README.md
@@ -0,0 +1,6 @@
+## Tree
+
+### 图解🧩
+
+[中序遍历非递归图解](https://github.com/Agility6/data-structure-c/blob/e23e9497485e10fb6ca18351960ff33aebe41a31/Tree/tree-traversal.cpp#L87)
+![inOrder](./img/inorder.png)
---
+++ Tree/application-tree.cpp
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#define OK 1
+#define NULL 0
+#define OVERFLOW -2
+
+/**
+  注意：该代码为伪代码!
+*/
+
+
+typedef int Status;
+typedef int TElemType;
+
+typedef struct BiNode {
+    TElemType data;
+    BiNode *lchild, *rchild;
+} BiNode *BiTree;
+
+
+// 1. 按先序遍历序列建立二叉树的二叉链表
+/**
+  输入二叉树的结点，空结点则用#
+*/
+Status CreateBiTree(BiTree &T) {
+
+  char ch;
+  scanf("%c", &ch);
+
+  if(ch == "#") {
+    T = NULL;
+  } else {
+    // 生成树
+    if(!(T = (BiTNode*)malloc(sizeof(BiTNode)))) {
+      exit(OVERFLOW)
+    }
+    T -> data = ch;
+    CreateBiTree(T -> data) // 构造左子树
+    CreateBiTree(T -> data) // 构造右子树
+  }
+
+  return OK; 
+  
+}
+
+// 2. 复制二叉树
+/**
+  如果是空树，递归结束
+  否则，申请新结点空间，复制根节点
+    - 递归复制左子树
+    - 递归复制右子树
+*/ 
+int Copy(BiTree T, BiTree &NewT) {
+
+  if(T = NULL) {
+    return NewT = NULL;
+    return 0;
+  } else {
+    NewT = (BiTNode*)malloc(sizeof(BiTNode))
+    NewT -> data = T -> data;
+    Copy(T -> lchild, NewT -> lchild);
+    Copy(T -> rchild, NewT -> rchild);
+  }
+
+}
+
+// 3. 计算二叉树深度
+/**
+  如果是空树，则深度为0
+  否则，递归计算左子树的深度记为m，递归计算右子树的深度记为n，
+  二叉树的深度则为m与n的较大者+1
+*/
+int Depth(BiTree T) {
+
+  if(T == NULL) {
+    return 0;
+  } else {
+    m = Depth(T -> lchild);
+    n = Depth(T -> rchild);
+    if(m>n) {
+      return (m+1);
+    } else {
+      return n + 1;
+    }
+  }
+
+}
+
+int main() {
+
+}
